ffc187f57ce2880dfd6eabbeae453e4dbb754e0a
Alexander Sieg <alex@xanderio.de>
2023-04-14 19:48:39 +0200

    flake: package for nix ecosystem

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     devenv.url = "github:cachix/devenv";
+    nix-npm-buildpackage.url = "github:serokell/nix-npm-buildpackage";
   };
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -16,11 +17,7 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      perSystem = {
-        pkgs,
-        lib,
-        ...
-      }: {
+      perSystem = { system, config, pkgs, lib,... }: {
         devenv.shells.default = {config, ...}: {
           languages = {
             php.enable = true;
@@ -89,6 +86,38 @@
           };
         };
         formatter = pkgs.alejandra;
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.nix-npm-buildpackage.overlays.default ];
+        };
+
+        packages =
+          let
+            inherit (builtins) substring;
+
+            mtime = inputs.self.lastModifiedDate;
+            date = "${substring 0 4 mtime}-${substring 4 2 mtime}-${substring 6 2 mtime}";
+            src = lib.cleanSource ./.;
+          in
+          rec {
+            node = pkgs.buildNpmPackage {
+              inherit src;
+              npmBuild = "npm run production";
+            };
+
+            php = pkgs.callPackage ./composer-project.nix { } src;
+
+            default = php.overrideAttrs (oldAttrs:
+              {
+                pname = "traewelling";
+                version = "unstable-${date}";
+                postInstall = ''
+                  # replace public folder with node build artifacts
+                  rm -rf $out/libexec/source/public
+                  cp -r ${node}/public $out/libexec/public
+                '';
+              });
+          };
       };
     };
 }
